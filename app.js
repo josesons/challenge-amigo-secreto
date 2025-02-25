4ac08a9481184327728526fa771714b631171324
Salvador Macias <salvadormacias@proton.me>
2025-02-24 22:43:43 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -3,16 +3,36 @@
 
 let amigos = [];
 
+// Implementa una función para agregar amigos
+
 function agregarAmigo() {
     let nombreDeAmigo = document.getElementById('amigo').value;
     if (nombreDeAmigo === '') {
         alert('Por favor, inserte un nombre.');
     } else {
+        // amigos.push(console.log(nombreDeAmigo)); 
         amigos.push(nombreDeAmigo); 
         limpiarCaja();
+        mostrarListaDeAmigos()
     }
 }
 
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
+
+// Implementa una función para actualizar la lista de amigos
+
+function mostrarListaDeAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let element = amigos[i];
+    
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        lista.appendChild(listaHTML);
+      }
+}
+
